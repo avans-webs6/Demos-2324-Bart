f3194f5398794ccdf6d5bea6cb670341557b7921
dibarto <github@dibarto.nl>
2024-04-24 15:30:10 +0200

    Added Festival model

---
+++ src/app/models/festival.model.ts
@@ -0,0 +1,11 @@
+export class Festival {
+    name: string;
+    date: string;
+    location: string;
+
+    constructor(name: string, date: string, location: string) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+    }
+}
