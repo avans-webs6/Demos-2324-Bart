04ce86af1aeabaa581f049c11e6a749e5e02e058
dibarto <github@dibarto.nl>
2024-05-26 22:42:54 +0200

    Demonstration of async pipe

--- src/app/festival-details/festival-details.component.ts
+++ src/app/festival-details/festival-details.component.ts
@@ -4,7 +4,7 @@ import { Festival } from '../models/festival.model';
 import { FestivalService } from '../festival.service';
 import { ActivatedRoute } from '@angular/router';
 
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-festival-details',
@@ -14,31 +14,17 @@ import { Subscription } from 'rxjs';
 export class FestivalDetailsComponent {
   selected_id: string = "";
 
-  subscription: Subscription | undefined;
-
-  festival: Festival | undefined;
-  organiser: string = "";
-  participants: string = "";
+  festival: Observable<Festival | undefined>;
+  organiser: Observable<string>;
+  participants: Observable<string[]>;
 
   constructor(private service: FestivalService, private route: ActivatedRoute) {
     this.selected_id = this.route.snapshot.paramMap.get('id') ?? "";
 
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.festival = this.service.getFestival(this.selected_id);
 
-    this.subscription = this.service.getFestival(this.selected_id).subscribe((festival: any) => {
-      this.festival = festival;
+    this.organiser = this.service.getFestivalOrganiser(this.selected_id);
 
-      if (festival) {
-        this.service.getFestivalOrganiser(this.selected_id).subscribe((organiser) => {
-          this.organiser = organiser;
-        })
-
-        this.service.getFestivalParticipants(this.selected_id).subscribe((participants) => {
-          this.participants = participants.join(', ');
-        })
-      }
-    });
+    this.participants = this.service.getFestivalParticipants(this.selected_id);
   }
 }
